9b67fbe9e7ddaf78e7f8c9c73da83e8ea76ba5d6
Akshat Garg <akshatg805@gmail.com>
2024-04-06 00:38:54 +0530

    Building 2nd Feature

---
+++ apps/macos-portfolio/app/_components/Terminal-Body.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+export default function TerminalBody() {
+  return (
+    <>
+      <div className="h-[92.5%] w-full bg-blue-500">
+        <p className="w-full h-fit">
+          Type "help" to show a list of available commands.
+        </p>
+        <div className="w-full h-fit">
+          <div className="w-2/12 bg-emerald-500">akshat-garg</div>
+          <input
+            className="w-10/12 bg-white"
+            type="text"
+            onChange={(event) => {
+              console.log("target->", event.target.value);
+            }}
+            onKeyDown={(event) => {
+              console.log("key->", event.key);
+              console.log("value->", event.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ apps/macos-portfolio/app/_components/Terminal.jsx
@@ -0,0 +1,13 @@
+import TerminalBody from "./Terminal-Body";
+import TerminalTitle from "./Terminal-Title";
+
+export default function Terminal() {
+  return (
+    <>
+      <div className="translate-x-1/4 translate-y-[10%] max-h-full max-w-full bg-orange-500 rounded-md min-h-10 min-w-64 w-4/6 h-3/4 drop-shadow-lg">
+        <TerminalTitle />
+        <TerminalBody />
+      </div>
+    </>
+  );
+}
