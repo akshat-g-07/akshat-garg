52070b6ab4e00b43720e12160141dc121f21d373
Akshat Garg <akshatg805@gmail.com>
2024-04-07 23:38:08 +0530

    Making progress on second MVP Feature
    ->Created EventListener file to keep event listener(s) at one place.
    ->Made progress in Terminal Body

--- apps/macos-portfolio/app/_components/Terminal-Body.jsx
+++ apps/macos-portfolio/app/_components/Terminal-Body.jsx
@@ -1,48 +1,73 @@
 "use client";
 
 import Image from "next/image";
-import Commands from "./Commands.json";
+import CommandsList from "./CommandsList.json";
 import { HelpCommand, AboutCommand } from "./Command-Values";
+import OnKeyDown from "./EventListener";
 
 export default function TerminalBody() {
-  console.log(Commands);
+  console.log(CommandsList);
   return (
     <>
-      <div id="terminal-body" className="h-[92.5%] w-full bg-blue-500">
+      <div
+        id="terminal-body"
+        className="h-[92.5%] w-full bg-blue-500 overflow-y-auto"
+        style={{
+          scrollbarColor: "rgb(75 85 99) rgb(31 41 55)", // thumb color track color
+          scrollbarWidth: "thin",
+        }}
+      >
         <p className="w-full h-fit">
           Type "help" to show a list of available commands.
         </p>
-        <div className="w-full h-fit flex">
-          <div className="w-2/12 flex items-center">
+        <div className="w-[98%] bg-green-500 h-fit flex terminal-command-line">
+          <div className="w-1/5 bg-red-500 flex items-center pl-3">
             <div className="w-fit bg-yellow-400 pr-2">akshat-garg</div>
-            <div className="h-6 w-5 relative">
+            <div className="h-6 w-5 bg-white relative">
               <Image src="/triangle.png" fill={true} alt="Terminal Triangle" />
             </div>
           </div>
           <input
-            className="w-10/12 bg-white focus:outline-none border-none"
+            className="w-4/5 bg-white focus:outline-none border-none"
             type="text"
             onKeyDown={(event) => {
-              console.log("key->", event.key);
-              console.log("value->", event.target.value);
-              if (event.key === "Enter") {
-                event.preventDefault();
+              OnKeyDown(event);
+              //   console.log("key->", event.key);
+              //   console.log("value->", event.target.value);
+              //   if (event.key === "Enter") {
+              //     event.preventDefault();
 
-                const node = document.createElement("div");
-                switch (event.target.value) {
-                  case "help":
-                    node.innerText = HelpCommand();
-                    break;
-                  case "about":
-                    node.innerText = AboutCommand();
+              //     const node = document.createElement("div");
+              //     switch (event.target.value) {
+              //       case "help":
+              //         node.innerText = HelpCommand();
+              //         break;
+              //       case "about":
+              //         node.innerText = AboutCommand();
 
-                    break;
+              //         break;
 
-                  default:
-                    break;
-                }
-                document.getElementById("terminal-body").appendChild(node);
-              }
+              //       default:
+              //         break;
+              //     }
+              //     document.getElementById("terminal-body").appendChild(node);
+              //     const clone = document
+              //       .getElementsByClassName("terminal-command-line")[0]
+              //       .cloneNode(true);
+
+              //     clone.querySelector("input").value = "";
+              //     clone
+              //       .querySelector("input")
+              //       .addEventListener("keydown", (event) => {
+              //         console.log(
+              //           "-------->key down clone",
+              //           event.target.value,
+              //           event.key
+              //         );
+              //       });
+
+              //     document.getElementById("terminal-body").appendChild(clone);
+              //   }
             }}
           />
         </div>
