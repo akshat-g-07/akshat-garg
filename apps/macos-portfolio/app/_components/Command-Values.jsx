32e91ac729b28891eeb3e4ec577961eea8cb18b0
Akshat Garg <akshatg805@gmail.com>
2024-04-09 16:38:35 +0530

    Implemented "skills" command

--- apps/macos-portfolio/app/_components/Command-Values.jsx
+++ apps/macos-portfolio/app/_components/Command-Values.jsx
@@ -9,21 +9,83 @@ export function About() {
 export function Skills() {
   const skillsNode = document.createElement("div");
   skillsNode.classList = "w-full h-fit bg-green-500 flex";
-  skillsNode.innerHTML = `<div class="h-fit w-1/2">
-      <div class="h-fit w-full flex">
-        <div class="h-1 w-1/5">
-          <img src="/skills/chakra.png" width="25" height="25"/>
-        </div>
-        <div class="h-fit w-4/5">Chakra UI</div>
-      </div>
+  skillsNode.innerHTML = `
+<div class="h-fit w-1/2">
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+      <img src="/skills/nextjs.png" width="15" height="15"/>
     </div>
-    <div class="h-fit w-1/2">
-    <div class="h-fit w-full flex">
-      <div class="h-1 w-1/5">
-        <img src="/skills/chakra.png" width="25" height="25"/>
-      </div>
-      <div class="h-fit w-4/5">Chakra UI</div>
+    <div class="h-full w-4/5">Next JS</div>
+  </div>
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+      <img src="/skills/nodejs.png" width="15" height="15"/>
     </div>
+  <div class="h-full w-4/5">Node JS</div>
+  </div>
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+      <img src="/skills/mongodb.png" width="15" height="15"/>
+    </div>
+    <div class="h-full w-4/5">Mongo DB</div>
+  </div>
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+      <img src="/skills/vite.png" width="15" height="15"/>
+    </div>
+    <div class="h-full w-4/5">Vite JS</div>
+  </div>
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+      <img src="/skills/tailwind.png" width="15" height="15"/>
+    </div>
+    <div class="h-full w-4/5">Tailwind CSS</div>
+  </div>
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+      <img src="/skills/git.png" width="15" height="15"/>
+    </div>
+    <div class="h-full w-4/5">Git</div>
+  </div>
+</div>
+<div class="h-fit w-1/2">
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+        <img src="/skills/reactjs.png" width="15" height="15"/>
+    </div>
+    <div class="h-full w-4/5">React JS</div>
+    </div>
+    <div class="h-6 w-full flex">
+      <div class="h-full w-1/5 flex items-center justify-center">
+        <img src="/skills/expressjs.png" width="15" height="15"/>
+      </div>
+      <div class="h-full w-4/5">Express JS</div>
+    </div>
+  <div class="h-6 w-full flex">
+    <div class="h-full w-1/5 flex items-center justify-center">
+      <img src="/skills/postgresql.png" width="15" height="15"/>
+    </div>
+    <div class="h-full w-4/5">PostgreSQL</div>
+  </div>
+  <div class="h-6 w-full flex">
+  <div class="h-full w-1/5 flex items-center justify-center">
+    <img src="/skills/prisma.png" width="15" height="15"/>
+  </div>
+  <div class="h-full w-4/5">Prisma</div>
+  </div>
+  <div class="h-6 w-full flex">
+  <div class="h-full w-1/5 flex items-center justify-center">
+    <img src="/skills/sass.png" width="15" height="15"/>
+  </div>
+  <div class="h-full w-4/5">SASS</div>
+  </div>
+  <div class="h-6 w-full flex">
+  <div class="h-full w-1/5 flex items-center justify-center">
+    <img src="/skills/github.png" width="15" height="15"/>
+  </div>
+  <div class="h-full w-4/5">Github</div>
+  </div>
+</div>
   </div>
    `;
   return skillsNode;
