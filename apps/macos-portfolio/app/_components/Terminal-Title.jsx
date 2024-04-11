0aa0f23f4da5edd5991463c76ec9487e913e847a
Akshat Garg <akshatg805@gmail.com>
2024-04-11 23:01:45 +0530

    4th Feature's Maximize Button Implemented

--- apps/macos-portfolio/app/_components/Terminal-Title.jsx
+++ apps/macos-portfolio/app/_components/Terminal-Title.jsx
@@ -15,7 +15,29 @@ export default function TerminalTitle() {
             }}
           />
           <div className="rounded-full cursor-pointer bg-yellow-500 size-3" />
-          <div className="rounded-full cursor-pointer bg-green-500 size-3" />
+          <div
+            className="rounded-full cursor-pointer bg-green-500 size-3"
+            onClick={() => {
+              const terminal = document.getElementById("terminal");
+              if (terminal.classList.contains("MAX_SIZE")) {
+                terminal.classList.remove("MAX_SIZE");
+                terminal.classList.remove("w-full");
+                terminal.classList.remove("h-full");
+                terminal.classList.add("w-4/6");
+                terminal.classList.add("h-3/4");
+                terminal.classList.add("translate-x-1/4");
+                terminal.classList.add("translate-y-[10%]");
+              } else {
+                terminal.classList.add("MAX_SIZE");
+                terminal.classList.add("w-full");
+                terminal.classList.add("h-full");
+                terminal.classList.remove("w-4/6");
+                terminal.classList.remove("h-3/4");
+                terminal.classList.remove("translate-x-1/4");
+                terminal.classList.remove("translate-y-[10%]");
+              }
+            }}
+          />
         </div>
         <p className="text-center h-full w-11/12">Akshat Garg</p>
       </div>
