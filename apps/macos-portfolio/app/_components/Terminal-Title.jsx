b3e5f03794b5e3441ce244b8f8ffbd23dc2e0418
Akshat Garg <akshatg805@gmail.com>
2024-04-11 22:45:45 +0530

    4th Feature's Close Button Implemented

--- apps/macos-portfolio/app/_components/Terminal-Title.jsx
+++ apps/macos-portfolio/app/_components/Terminal-Title.jsx
@@ -1,11 +1,21 @@
+"use client";
+
 export default function TerminalTitle() {
   return (
     <>
       <div className="h-[7.5%] w-full flex bg-lime-300 rounded-t-md">
         <div className="flex w-1/12 h-full items-center justify-evenly bg-black">
-          <div className="rounded-full bg-red-500 size-3" />
-          <div className="rounded-full bg-yellow-500 size-3" />
-          <div className="rounded-full bg-green-500 size-3" />
+          <div
+            className="rounded-full cursor-pointer bg-red-500 size-3"
+            onClick={() => {
+              document.getElementById("terminal").remove();
+              sessionStorage.removeItem("commandIndx");
+              sessionStorage.removeItem("commandsHistory");
+              sessionStorage.removeItem("currentCommand");
+            }}
+          />
+          <div className="rounded-full cursor-pointer bg-yellow-500 size-3" />
+          <div className="rounded-full cursor-pointer bg-green-500 size-3" />
         </div>
         <p className="text-center h-full w-11/12">Akshat Garg</p>
       </div>
--- apps/macos-portfolio/app/_components/Terminal.jsx
+++ apps/macos-portfolio/app/_components/Terminal.jsx
@@ -4,7 +4,10 @@ import TerminalTitle from "./Terminal-Title";
 export default function Terminal() {
   return (
     <>
-      <div className="translate-x-1/4 translate-y-[10%] max-h-full max-w-full bg-orange-500 rounded-md min-h-10 min-w-64 w-4/6 h-3/4 drop-shadow-lg">
+      <div
+        id="terminal"
+        className="translate-x-1/4 translate-y-[10%] max-h-full max-w-full bg-orange-500 rounded-md min-h-10 min-w-64 w-4/6 h-3/4 drop-shadow-lg"
+      >
         <TerminalTitle />
         <TerminalBody />
       </div>
