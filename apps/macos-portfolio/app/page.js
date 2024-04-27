0a4a262fabd937f5c793fab2946f4cf6230de82d
Akshat Garg <akshatg805@gmail.com>
2024-04-27 23:55:56 +0530

    Fixing redirection
    -> Putting whole logic inside useffect for it to run on client side only. else it throws build error.
    -> Giving an alert before redirecting. So that user knows.

--- apps/macos-portfolio/app/page.js
+++ apps/macos-portfolio/app/page.js
@@ -2,13 +2,19 @@
 
 import LockScreen from "./_components/Lock-Screen";
 import Body from "./_components/Body";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  if (screen.width <= 390) {
-    window.location.href = "https://threejs-portfolio.akshat-garg.com";
-  }
   const [unlocked, setUnlocked] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 390) {
+      alert(
+        "This particular portfolio is best viewed in bigger screens (Laptop/PC/Tablet). Redirecting you to secondary portfolio."
+      );
+      window.location.href = "https://threejs-portfolio.akshat-garg.com/";
+    }
+  }, []);
   return (
     <>
       <LockScreen setUnlocked={setUnlocked} />
