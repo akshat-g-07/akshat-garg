9a1b9c28ffaefbc70a1eeca0f0f4366d285b9fe7
Akshat Garg <akshatg805@gmail.com>
2024-04-30 22:59:21 +0530

    Publishing Repo-Details Package & Declaring SKills
    -> Published portfolio-details package
    -> Installed package in our portfolio apps
    -> Declared Skills in our package

--- apps/threejs-portfolio/src/App.jsx
+++ apps/threejs-portfolio/src/App.jsx
@@ -1,7 +1,11 @@
 import AllSections from "./sections/AllSections";
 import "./App.css";
+import { Skills } from "@repo/portfolio-details";
 
 function App() {
+  Skills.map((skill) => {
+    console.log(skill.name);
+  });
   return <AllSections />;
 }
 
