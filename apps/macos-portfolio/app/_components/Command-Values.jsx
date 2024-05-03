0dae22f6daae902dbb8cab4990c4dcbef984da28
Akshat Garg <akshatg805@gmail.com>
2024-05-03 15:34:09 +0530

    Integrating package with macos portfolio
    -> Only skills as other variables are hardcoded and v difficult to fix now.

--- apps/macos-portfolio/app/_components/Command-Values.jsx
+++ apps/macos-portfolio/app/_components/Command-Values.jsx
@@ -1,4 +1,6 @@
-export function Help() {
+import { Skills } from "@repo/portfolio-details";
+
+export function HelpFunction() {
   return `<table style="width:50%; text-align: left;">
   <tr>
     <th>Command</th>
@@ -59,104 +61,39 @@ export function Help() {
 </table>`;
 }
 
-export function About() {
+export function AboutFunction() {
   return "Hey there!\nI am Akshat Garg, alumnus of NIT Patna. I am an experienced web developer who really enjoys creating websites.  I can endlessly talk about web development using different JS libraries/frameworks. I really thrive to develop something meaningful!";
 }
 
-export function Skills() {
+export function SkillsFunction() {
   const skillsNode = document.createElement("div");
   skillsNode.classList = "w-full h-fit flex";
   skillsNode.innerHTML = `
-<div class="h-fit w-1/2">
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-      <img src="/skills/nextjs.png" width="15" height="15"/>
-    </div>
-    <div class="h-full w-4/5">Next JS</div>
-  </div>
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-      <img src="/skills/nodejs.png" width="15" height="15"/>
-    </div>
-  <div class="h-full w-4/5">Node JS</div>
-  </div>
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-      <img src="/skills/mongodb.png" width="15" height="15"/>
-    </div>
-    <div class="h-full w-4/5">Mongo DB</div>
-  </div>
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-      <img src="/skills/vite.png" width="15" height="15"/>
-    </div>
-    <div class="h-full w-4/5">Vite JS</div>
-  </div>
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-      <img src="/skills/tailwind.png" width="15" height="15"/>
-    </div>
-    <div class="h-full w-4/5">Tailwind CSS</div>
-  </div>
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-      <img src="/skills/git.png" width="15" height="15"/>
-    </div>
-    <div class="h-full w-4/5">Git</div>
-  </div>
-</div>
-<div class="h-fit w-1/2">
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-        <img src="/skills/reactjs.png" width="15" height="15"/>
-    </div>
-    <div class="h-full w-4/5">React JS</div>
-    </div>
-    <div class="h-6 w-full flex">
-      <div class="h-full w-1/5 flex items-center justify-center">
-        <img src="/skills/expressjs.png" width="15" height="15"/>
+  <div class="w-full h-fit flex flex-wrap">
+    ${Skills.map(
+      (skill) => `
+      <div class="h-6 w-1/2 flex">
+        <div class="h-full w-1/5 flex items-center justify-center">
+          <img src="/skills/${skill.imgName}" width="15" height="15" />
+        </div>
+        <div class="h-full w-4/5">${skill.name}</div>
       </div>
-      <div class="h-full w-4/5">Express JS</div>
-    </div>
-  <div class="h-6 w-full flex">
-    <div class="h-full w-1/5 flex items-center justify-center">
-      <img src="/skills/postgresql.png" width="15" height="15"/>
-    </div>
-    <div class="h-full w-4/5">PostgreSQL</div>
+    `
+    ).join("")}
   </div>
-  <div class="h-6 w-full flex">
-  <div class="h-full w-1/5 flex items-center justify-center">
-    <img src="/skills/prisma.png" width="15" height="15"/>
-  </div>
-  <div class="h-full w-4/5">Prisma</div>
-  </div>
-  <div class="h-6 w-full flex">
-  <div class="h-full w-1/5 flex items-center justify-center">
-    <img src="/skills/sass.png" width="15" height="15"/>
-  </div>
-  <div class="h-full w-4/5">SASS</div>
-  </div>
-  <div class="h-6 w-full flex">
-  <div class="h-full w-1/5 flex items-center justify-center">
-    <img src="/skills/github.png" width="15" height="15"/>
-  </div>
-  <div class="h-full w-4/5">Github</div>
-  </div>
-</div>
-  </div>
-   `;
+  `;
   return skillsNode;
 }
 
-export function Education() {
+export function EducationFunction() {
   return "I'm proud to be an alumnus of NIT Patna, where I completed my B.Tech degree from 2017 to 2021.";
 }
 
-export function Experience() {
+export function ExperienceFunction() {
   return "-> I have been making a difference at Infosys Limited on the role of Specialist Programmer since Aug'21.";
 }
 
-export function Projects() {
+export function ProjectsFunction() {
   const projectsNode = document.createElement("div");
   projectsNode.classList = "w-full h-fit";
   projectsNode.innerHTML = ` <div class="w-full h-fit">
@@ -260,7 +197,7 @@ export function Projects() {
   return projectsNode;
 }
 
-export function Achievements() {
+export function AchievementsFunction() {
   return `↪ Make-a-thon 16 | Finalists
   ≕ Successfully led a team of 4 to the finals of Infosys’s prestigious Hackathon event where brightest minds showcase their skills.
   ≕  Leveraged expertise in full-stack development and picked a PS where we had to develop a Bidding Platform from scratch, showcasing proficiency in both frontend and backend.
@@ -268,6 +205,6 @@ export function Achievements() {
   `;
 }
 
-export function Mail() {
+export function MailFunction() {
   return "akshatg805@gmail.com";
 }
