c313cd3c6531f5a2b34669c6a9fa4dbc794b976c
Akshat Garg <akshatg805@gmail.com>
2024-09-08 12:32:09 +0530

    feat: Noteworthy Ninja Privacy Policy

---
+++ apps/noteworthy-ninja-privacy-policy/app/privacy-policy/page.jsx
@@ -0,0 +1,79 @@
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <div className="size-full text-white bg-stone-700 min-h-screen p-10">
+        <h1 className="text-3xl font-bold w-full text-center">
+          Privacy Policy for Noteworthy Ninja
+        </h1>
+        <b>Effective Date:</b> 03/Sept/2024
+        <p>
+          Thank you for choosing to use Noteworthy Ninja. This Privacy Policy
+          outlines how we collect, use, and protect your personal information
+          when you use our Chrome extension.
+        </p>
+        <h2 className="text-xl font-semibold">1. Information We Collect</h2>
+        <p>
+          When you use Noteworthy Ninja, we may collect the following personal
+          information from you through Google OAuth 2.0:
+        </p>
+        <ul className="list-disc list-inside">
+          <li>Email Address: Used to identify and communicate with you.</li>
+          <li>
+            Name: Used to personalize your experience within the extension.
+          </li>
+          <li>
+            Profile Picture: Displayed within the extension for a personalized
+            experience.
+          </li>
+        </ul>
+        <h2 className="text-xl font-semibold">
+          2. How We Use Your Information
+        </h2>
+        <p>
+          The personal information we collect is used for the following
+          purposes:
+        </p>
+        <ul className="list-disc list-inside">
+          <li>
+            Authentication: To verify your identity through Google OAuth 2.0.
+          </li>
+          <li>
+            Personalization: To display your name and profile picture within the
+            extension.
+          </li>
+          <li>
+            Communication: To send you notifications or updates related to the
+            extension.
+          </li>
+        </ul>
+        <h2 className="text-xl font-semibold">3. Data Storage and Security</h2>
+        <p>
+          We are committed to protecting your personal information. Your data is
+          stored securely, and we implement various security measures to ensure
+          that your information is not accessed, disclosed, altered, or
+          destroyed by unauthorized individuals.
+        </p>
+        <h2 className="text-xl font-semibold">4. Sharing Your Information</h2>
+        <p>
+          We do not sell, trade, or otherwise transfer your personal information
+          to outside parties.
+        </p>
+        <h2 className="text-xl font-semibold">5. Your Consent</h2>
+        <p>By using Noteworthy Ninja, you consent to our Privacy Policy.</p>
+        <h2 className="text-xl font-semibold">
+          6. Changes to Our Privacy Policy
+        </h2>
+        <p>
+          We may update our Privacy Policy from time to time. We will notify you
+          of any changes by posting the new Privacy Policy on this page. You are
+          advised to review this Privacy Policy periodically for any changes.
+        </p>
+        <h2 className="text-xl font-semibold">7. Contact Us</h2>
+        <p>
+          If you have any questions about this Privacy Policy, please contact us
+          at `codesakshat@gmail.com`.
+        </p>
+      </div>
+    </>
+  );
+}
