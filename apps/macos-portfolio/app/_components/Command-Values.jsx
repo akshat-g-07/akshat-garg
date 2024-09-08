2b32c9082c197ae776df57b93a5cf25e67316173
Akshat Garg <akshatg805@gmail.com>
2024-09-08 13:21:41 +0530

    feat: adding achievement of makeathon17

--- apps/macos-portfolio/app/_components/Command-Values.jsx
+++ apps/macos-portfolio/app/_components/Command-Values.jsx
@@ -200,8 +200,12 @@ export function ProjectsFunction() {
 export function AchievementsFunction() {
   return `↪ Make-a-thon 16 | Finalists
   ≕ Successfully led a team of 4 to the finals of Infosys’s prestigious Hackathon event where brightest minds showcase their skills.
-  ≕  Leveraged expertise in full-stack development and picked a PS where we had to develop a Bidding Platform from scratch, showcasing proficiency in both frontend and backend.
+  ≕ Leveraged expertise in full-stack development and picked a PS where we had to develop a Bidding Platform from scratch, showcasing proficiency in both frontend and backend.
   ≕ Designed and implemented the platform using a Microservices architecture to build a robust product.
+  ↪ Make-a-thon 17 | Semi-Finalists
+  ≕ Created a team of 5 and led them from the front to the semi-finals of the event.
+  ≕ This time picked a PS where we had to develop a Copilot for the Insurance Domain, to help the testers with their monotonous tasks.
+  ≕ Applied prompt engineering techniques to train the model, translating natural language inputs into user-friendly actions.
   `;
 }
 
--- packages/portfolio-details/Achievements/index.js
+++ packages/portfolio-details/Achievements/index.js
@@ -8,6 +8,15 @@ const Achievements = [
       "Designed and implemented the platform using a Microservices architecture to build a robust product.",
     ],
   },
+  {
+    title: "Make-a-thon 17",
+    position: "Semi-Finalists",
+    details: [
+      "Created a team of 5 and led them from the front to the semi-finals of the event.",
+      "This time picked a PS where we had to develop a Copilot for the Insurance Domain, to help the testers with their monotonous tasks.",
+      "Applied prompt engineering techniques to train the model, translating natural language inputs into user-friendly actions.",
+    ],
+  },
 ];
 
 export default Achievements;
