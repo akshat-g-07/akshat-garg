d7655d71a46e7c0a48cb3fe450a617c860db0c63
Akshat Garg <akshatg805@gmail.com>
2025-04-13 22:27:35 +0530

    feat: hero section

--- apps/treklicious-fe/src/pages/home/index.jsx
+++ apps/treklicious-fe/src/pages/home/index.jsx
@@ -1,3 +1,11 @@
+import HeroSection from "@/components/home/hero-section";
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <section className="w-full min-h-screen h-[2000px] flex flex-col items-center">
+        <HeroSection />
+      </section>
+    </>
+  );
 }
