4a728b627b357a9d6a5c716a69c7317bd9baec3b
Akshat Garg <akshatg805@gmail.com>
2025-04-16 23:05:27 +0530

    feat: state section

---
+++ apps/treklicious-fe/src/components/home/state.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState, useMemo } from "react";
+import SectionTemplate from "./section-template";
+import Treks from "@/assets/Treks.json";
+import { cn } from "@/lib/utils";
+import Tile from "../common/tile";
+
+export default function State() {
+  const imgSrc =
+    "https://wallpapers.com/images/hd/hiking-on-foggy-mountain-g7bz0k4c45e2zmgt.jpg";
+
+  // MARK: db
+  // or maybe fetch all the treks grouped by states
+  const states = useMemo(
+    () => [
+      "Uttarakhand",
+      "Himachal Pradesh",
+      "Jammu and Kashmir",
+      "Karnataka",
+      "Maharashtra",
+    ],
+    []
+  );
+
+  const [index, setIndex] = useState(0);
+
+  const [treks, setTreks] = useState(
+    Treks.filter((trek) => trek.state === states[index])
+  );
+
+  useEffect(() => {
+    setTreks(Treks.filter((trek) => trek.state === states[index]));
+  }, [index, states]);
+
+  return (
+    <SectionTemplate
+      bgImgUrl={imgSrc}
+      subTitle="to explore state treasures"
+      className="bg-auto bg-top"
+    >
+      <div className="w-full flex flex-wrap items-center justify-center gap-4 mt-6">
+        {states.map((state, indx) => (
+          <div
+            key={indx}
+            className={cn(
+              "rounded px-4 py-2 border text-white border-white/75 cursor-pointer",
+              index === indx && "font-medium border-amber-500 text-amber-500",
+              "hover:border-amber-500 hover:text-amber-500"
+            )}
+            onClick={() => {
+              setIndex(indx);
+            }}
+          >
+            {state}
+          </div>
+        ))}
+      </div>
+      <div className="w-full flex flex-wrap items-center justify-center gap-4 mt-6 min-h-[500px]">
+        {treks.map((trek, indx) => (
+          <Tile
+            key={indx}
+            imgSrc={trek.img}
+            alt={trek.name.toLowerCase()}
+            title={trek.name}
+            link={trek.name.toLowerCase()}
+          />
+        ))}
+      </div>
+    </SectionTemplate>
+  );
+}
--- apps/treklicious-fe/src/pages/home/index.jsx
+++ apps/treklicious-fe/src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import CallToAction from "@/components/home/call-to-action";
 import HeroSection from "@/components/home/hero-section";
 import Season from "@/components/home/season";
+import State from "@/components/home/state";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <HeroSection />
         <CallToAction />
         <Season />
+        <State />
       </section>
     </>
   );
