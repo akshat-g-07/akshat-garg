276bee4a4af8bf31ac8bcd08310f017858f300bb
Akshat Garg <akshatg805@gmail.com>
2025-04-24 23:14:38 +0530

    feat: sign up page router setup

--- apps/treklicious-fe/src/routes/index.js
+++ apps/treklicious-fe/src/routes/index.js
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import Home from "@/pages/home";
-
+import SignUp from "@/pages/sign-up";
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+  },
+  {
+    path: "/sign-up",
+    Component: SignUp,
   },
 ]);
