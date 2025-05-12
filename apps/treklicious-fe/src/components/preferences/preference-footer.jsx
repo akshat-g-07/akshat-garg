c983a534644e987c6b4df0f7d69bb0749bcc5ec8
Akshat Garg <akshatg805@gmail.com>
2025-05-12 12:31:27 +0530

    feat: preference footer component

---
+++ apps/treklicious-fe/src/components/preferences/preference-footer.jsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+
+export default function PreferenceFooter({ activeIndx, handleClick }) {
+  return (
+    <>
+      <div className="md:col-span-2 flex w-full justify-between items-center-safe h-fit gap-x-2 md:gap-x-4 lg:gap-x-10">
+        <Button
+          size="lg"
+          disabled={activeIndx === 0}
+          className="md:col-span-2 w-fit justify-self-center-safe cursor-pointer"
+          onClick={() => {
+            handleClick(activeIndx - 1);
+          }}
+        >
+          Back
+        </Button>
+        <Button
+          size="lg"
+          className="md:col-span-2 w-fit justify-self-center-safe cursor-pointer"
+          onClick={() => {
+            handleClick(activeIndx + 1);
+          }}
+        >
+          {activeIndx === 2 ? "Finish" : "Next"}
+        </Button>
+      </div>
+    </>
+  );
+}
--- apps/treklicious-fe/src/pages/preferences/index.jsx
+++ apps/treklicious-fe/src/pages/preferences/index.jsx
@@ -1,14 +1,23 @@
 import { Auth, AuthBody } from "@/components/common/auth-setup";
+import PreferenceFooter from "@/components/preferences/preference-footer";
 import Stepper from "@/components/preferences/stepper";
 import { useState } from "react";
 
 export default function Preferences() {
   const [activeIndx, setActiveIndx] = useState(0);
+
+  const handleClick = (indx) => {
+    if (indx === 3) {
+      //then do something api and reroute to home page
+    } else setActiveIndx(indx);
+  };
+
   return (
     <>
       <Auth>
         <AuthBody>
           <Stepper activeIndx={activeIndx} />
+          <PreferenceFooter activeIndx={activeIndx} handleClick={handleClick} />
         </AuthBody>
       </Auth>
     </>
