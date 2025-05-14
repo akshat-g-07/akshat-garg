40b65e17fc29f27e6cafdb7b0f4ebdfd054e5072
Akshat Garg <akshatg805@gmail.com>
2025-05-14 12:00:18 +0530

    feat: utils

---
+++ apps/treklicious-fe/src/lib/component-generator.js
@@ -0,0 +1,83 @@
+import { seasonLines, stateLines, difficultyLines } from "./section-heading";
+import TrekCategoryGenerator from "./trek-category-generator";
+import Treks from "@/assets/Treks.json";
+
+const usedSectionHeads = [];
+
+const getUniqueSectionHead = (lines) => {
+  let index = Math.floor(Math.random() * lines.length);
+  while (usedSectionHeads.includes(lines[index])) {
+    index = Math.floor(Math.random() * lines.length);
+  }
+  return lines[index];
+};
+
+const GenerateStateLine = (stateName) => {
+  const value = getUniqueSectionHead(stateLines);
+  usedSectionHeads.push(value);
+
+  const sectionArray = Treks.filter((trek) => trek.state === stateName).slice(
+    0,
+    10
+  );
+
+  return {
+    filterParameter: "state",
+    filterValue: stateName,
+    sectionHeadValue: value + stateName,
+    sectionArrayValue: sectionArray,
+  };
+};
+
+const GenerateSeasonLine = (seasonName) => {
+  const value = getUniqueSectionHead(seasonLines);
+  usedSectionHeads.push(value);
+
+  const sectionArray = Treks.filter((trek) => trek.season === seasonName).slice(
+    0,
+    10
+  );
+
+  return {
+    filterParameter: "season",
+    filterValue: seasonName,
+    sectionHeadValue: value + seasonName,
+    sectionArrayValue: sectionArray,
+  };
+};
+
+const GenerateDifficultyLine = (difficultyName) => {
+  const value = getUniqueSectionHead(difficultyLines);
+  usedSectionHeads.push(value);
+
+  const sectionArray = Treks.filter(
+    (trek) => trek.difficulty === difficultyName
+  ).slice(0, 10);
+
+  return {
+    filterParameter: "difficulty",
+    filterValue: difficultyName,
+    sectionHeadValue: value,
+    sectionArrayValue: sectionArray + difficultyName + " Treks",
+  };
+};
+
+const ComponentGenerator = () => {
+  const trekCategory = TrekCategoryGenerator(Treks);
+  const componentValue = [];
+
+  trekCategory.forEach((category) => {
+    const [[key, value]] = Object.entries(category);
+    componentValue.push(
+      key === "state"
+        ? GenerateStateLine(value)
+        : key === "season"
+          ? GenerateSeasonLine(value)
+          : GenerateDifficultyLine(value)
+    );
+  });
+
+  return componentValue;
+};
+
+export default ComponentGenerator;
