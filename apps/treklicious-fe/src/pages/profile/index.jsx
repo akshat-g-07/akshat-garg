4579dd465f6316f9e913329c96fc0e509044430b
Akshat Garg <akshatg805@gmail.com>
2025-05-15 13:23:12 +0530

    feat: profile form

--- apps/treklicious-fe/src/pages/profile/index.jsx
+++ apps/treklicious-fe/src/pages/profile/index.jsx
@@ -1,5 +1,21 @@
 import SettingSetup from "@/components/common/setting-setup";
+import ProfileForm from "@/components/profile/profile-form";
 
 export default function Profile() {
-  return <SettingSetup>This is profile page</SettingSetup>;
+  return (
+    <SettingSetup>
+      <ProfileForm
+        defaultFirstName={"defaultFirstName"}
+        defaultLastName={"defaultLastName"}
+        defaultEmail={"defaultEmail"}
+        defaultUserName={"defaultUserName"}
+      />
+      <h3
+        id="preferences"
+        className="pb-2 border-b-2 border-black w-full text-xl font-semibold cursor-default"
+      >
+        Preferences
+      </h3>
+    </SettingSetup>
+  );
 }
