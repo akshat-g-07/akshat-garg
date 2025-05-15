e353059e2d901bf61c502546ebc5219ec95fa792
Akshat Garg <akshatg805@gmail.com>
2025-05-15 23:23:05 +0530

    feat: utils for date-time

---
+++ apps/treklicious-be/utils/date-time.js
@@ -0,0 +1,28 @@
+function GetDate() {
+  return new Date().toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+}
+
+function GetDateTime() {
+  return new Date().toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
+
+function GetTime() {
+  return new Date().toLocaleString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
+
+module.exports = { GetDate, GetDateTime, GetTime };
