b382df55481eab4ab14f25971e4cd8e3c9a9e97c
Akshat Garg <akshatg805@gmail.com>
2025-05-15 22:56:01 +0530

    feat: db setup

--- apps/treklicious-be/index.js
+++ apps/treklicious-be/index.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3400;
 
+const mongoose = require("mongoose");
+const dbConnect = require("./config/dbConn");
+
+dbConnect();
+
 // This is to know if server is up and running
 app.get("/", (req, res) =>
   res.status(200).send(`Server is running on port ${port} on ${new Date()}`)
 );
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+mongoose.connection.once("open", () => {
+  console.log("Mongoose Connected");
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`Error in mongoose connection: ${err}`);
 });
