ec075f80a12b2c58f260d0b9ee394585710f7eb5
Akshat Garg <akshatg805@gmail.com>
2025-05-15 22:46:06 +0530

    feat: server info

--- apps/treklicious-be/index.js
+++ apps/treklicious-be/index.js
@@ -1,2 +1,12 @@
-console.log("hii");
-console.log(process.env.K1);
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3400;
+
+// This is to know if server is up and running
+app.get("/", (req, res) =>
+  res.status(200).send(`Server is running on port ${port} on ${new Date()}`)
+);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
