877b2c1aa3943fe3c466b1ed9896ac2395107951
Akshat Garg <akshatg805@gmail.com>
2025-05-15 16:37:55 +0530

    feat: header filters

--- apps/treklicious-fe/src/pages/explore-all/index.jsx
+++ apps/treklicious-fe/src/pages/explore-all/index.jsx
@@ -1,7 +1,28 @@
+import Treks from "@/assets/Treks.json";
+import BackButton from "@/components/common/back-button";
 import { useLocation } from "react-router";
+import { useState } from "react";
+import Filters from "@/components/explore-all/filters";
 
 export default function ExploreAll() {
   const location = useLocation();
-  console.log(location.state);
-  return <div>Explore All</div>;
+  const { filterParameter, filterValue } = location.state || {};
+  const [{ filterParam, filterVal }, setFilters] = useState({
+    filterParam: filterParameter || "",
+    filterVal: filterValue || "",
+  });
+
+  return (
+    <section>
+      <header className="w-full h-fit bg-[#6495ed] flex py-6 px-8">
+        <BackButton />
+
+        <Filters
+          filterParam={filterParam}
+          filterVal={filterVal}
+          setFilters={setFilters}
+        />
+      </header>
+    </section>
+  );
 }
