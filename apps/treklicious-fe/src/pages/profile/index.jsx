80a40e47b452877892703cbb394d7127ab0e5d29
Akshat Garg <akshatg805@gmail.com>
2025-05-15 14:27:00 +0530

    feat: preference form

--- apps/treklicious-fe/src/pages/profile/index.jsx
+++ apps/treklicious-fe/src/pages/profile/index.jsx
@@ -1,4 +1,5 @@
 import SettingSetup from "@/components/common/setting-setup";
+import PreferencesForm from "@/components/profile/preferences-form";
 import ProfileForm from "@/components/profile/profile-form";
 
 export default function Profile() {
@@ -10,12 +11,11 @@ export default function Profile() {
         defaultEmail={"defaultEmail"}
         defaultUserName={"defaultUserName"}
       />
-      <h3
-        id="preferences"
-        className="pb-2 border-b-2 border-black w-full text-xl font-semibold cursor-default"
-      >
-        Preferences
-      </h3>
+      <PreferencesForm
+        defaultState={"Uttarakhand"}
+        defaultSeason={"Summer"}
+        defaultDifficulty={"Easy"}
+      />
     </SettingSetup>
   );
 }
