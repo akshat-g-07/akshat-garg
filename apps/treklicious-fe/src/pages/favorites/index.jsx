528477f29bde68d40701d73a8400e540bb2ea598
Akshat Garg <akshatg805@gmail.com>
2025-05-15 15:51:09 +0530

    feat: favorite page

--- apps/treklicious-fe/src/pages/favorites/index.jsx
+++ apps/treklicious-fe/src/pages/favorites/index.jsx
@@ -1,3 +1,15 @@
+import SettingSetup from "@/components/common/setting-setup";
+import Treks from "@/assets/Treks.json";
+import FavoriteTreks from "@/components/favorites/favorite-treks";
+
 export default function Favorites() {
-  return <div></div>;
+  return (
+    <SettingSetup>
+      <h2 className="pb-2 border-b-2 border-black w-full text-3xl font-semibold cursor-default">
+        Your favorites are:
+      </h2>
+
+      <FavoriteTreks treks={Treks} />
+    </SettingSetup>
+  );
 }
