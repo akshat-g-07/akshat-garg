1231fb62008008731843f3dc61278dc90c5299ba
Akshat Garg <akshatg805@gmail.com>
2025-05-16 18:45:11 +0530

    feat: auth routes

---
+++ apps/treklicious-be/routes/v1/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+// login route
+router.post("/login", () => {});
+// refesh route
+router.get("/refresh", () => {});
+// signup route
+router.post("/signup", () => {});
+// logout route
+router.post("/logout", () => {});
+
+module.exports = router;
