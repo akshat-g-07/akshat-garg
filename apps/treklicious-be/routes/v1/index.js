d544942bda2d056ca86957cb2ae99af4e6cb48cc
Akshat Garg <akshatg805@gmail.com>
2025-05-16 18:55:33 +0530

    feat: user routes

--- apps/treklicious-be/routes/v1/index.js
+++ apps/treklicious-be/routes/v1/index.js
@@ -7,4 +7,7 @@ router.use("/trek", trekRoutes);
 const authRoutes = require("./auth");
 router.use("/auth", authRoutes);
 
+const userRoutes = require("./user");
+router.use("/user", userRoutes);
+
 module.exports = router;
---
+++ apps/treklicious-be/routes/v1/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+// user profile routes get request put request
+router
+  .route("/profile")
+  .get(() => {})
+  .put(() => {});
+
+// user preferences routes get request put request
+router
+  .route("/preferences")
+  .get(() => {})
+  .put(() => {});
+
+// user favorites routes get request post request to add delete req to remove
+router
+  .route("/favorites")
+  .get(() => {})
+  .post(() => {})
+  .delete(() => {});
+
+module.exports = router;
