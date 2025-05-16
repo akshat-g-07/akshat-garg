c824e8f4d7357d85099e0ba68b538509ff2967b5
Akshat Garg <akshatg805@gmail.com>
2025-05-16 17:47:23 +0530

    chores: clean up

--- apps/treklicious-be/health/index.js
+++ apps/treklicious-be/health/index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const express = require("express");
 const { GetDateTime } = require("../utils/date-time");
 const router = express.Router();
-const logger = require("../utils/logger");
 
 function getDbState() {
   const connectionState = mongoose.connection.readyState;
@@ -29,11 +28,6 @@ function getDbState() {
 }
 
 router
-  .use((req, res, next) => {
-    logger.log("req.baseUrl");
-    logger.log(req.baseUrl);
-    next();
-  })
   .get("/", (req, res) => {
     res.status(200).send({
       server: "Running",
