3b8392a1388f564ad090b27fbac244073619ddce
Akshat Garg <akshatg805@gmail.com>
2025-05-17 11:29:00 +0530

    feat: rate-limiter middleware

--- apps/treklicious-be/middleware/index.js
+++ apps/treklicious-be/middleware/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const logRequest = require("./logRequest");
 router.use(logRequest);
 
+const rateLimiter = require("./rate-limiter");
+router.use(rateLimiter);
+
 module.exports = router;
