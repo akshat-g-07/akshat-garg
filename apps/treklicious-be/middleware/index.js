0c64fb6a4ad8e619f3a80593821346aecb97c68a
Akshat Garg <akshatg805@gmail.com>
2025-05-17 11:44:53 +0530

    feat: verify-jwt middleware

--- apps/treklicious-be/middleware/index.js
+++ apps/treklicious-be/middleware/index.js
@@ -7,4 +7,7 @@ router.use(logRequest);
 const rateLimiter = require("./rate-limiter");
 router.use(rateLimiter);
 
+const verifyJWT = require("./verify-jwt");
+router.use(verifyJWT);
+
 module.exports = router;
