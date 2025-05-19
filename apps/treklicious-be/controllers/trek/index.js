12935879e29fefcfb832d998d15c8f142725a389
Akshat Garg <akshatg805@gmail.com>
2025-05-19 23:40:27 +0530

    feat: tile info state controller

--- apps/treklicious-be/controllers/trek/index.js
+++ apps/treklicious-be/controllers/trek/index.js
@@ -1,5 +1,6 @@
 const AllTreks = require("./all-treks");
 const TileInfo = require("./tile-info");
 const TrekByID = require("./trek-by-id");
+const TileInfoState = require("./tile-info-state");
 
-module.exports = { AllTreks, TrekByID, TileInfo };
+module.exports = { AllTreks, TrekByID, TileInfo, TileInfoState };
--- apps/treklicious-be/routes/v1/trek.js
+++ apps/treklicious-be/routes/v1/trek.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const { AllTreks, TrekByID, TileInfo } = require("../../controllers/trek");
+const {
+  AllTreks,
+  TrekByID,
+  TileInfo,
+  TileInfoState,
+} = require("../../controllers/trek");
 const router = express.Router();
 
 router.get("/", AllTreks);
 router.get("/trekID/:trekID", TrekByID);
 router.get("/tileInfo", TileInfo);
-// get title, state, image, uuid of all treks
-router.get("/tileInfoWithState", () => {});
+router.get("/tileInfoWithState", TileInfoState);
 // get uuid and name of all treks
 router.get("/allNames", () => {});
 // get random trek for dashboard page
