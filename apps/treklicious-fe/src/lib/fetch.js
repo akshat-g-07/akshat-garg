65f18acfbbb24ad16379534558a35474c2fc5e4b
Akshat Garg <akshatg805@gmail.com>
2025-05-21 17:28:20 +0530

    chores: cleaning up

--- apps/treklicious-fe/src/lib/fetch.js
+++ apps/treklicious-fe/src/lib/fetch.js
@@ -1,9 +1,4 @@
 export default async function (...args) {
   const res = await fetch(...args);
-  console.log("res fetch", res);
-
-  const json = await res.json();
-
-  console.log("res fetch1", json);
-  return json;
+  return await res.json();
 }
