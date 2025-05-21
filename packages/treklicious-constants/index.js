468a2818067a5986bc4e82e71788db4111d5a6f4
Akshat Garg <akshatg805@gmail.com>
2025-05-21 12:09:17 +0530

    chores: responses

--- packages/treklicious-constants/index.js
+++ packages/treklicious-constants/index.js
@@ -2,4 +2,6 @@ export const BAD_REQUEST_RESPONSE = "Bad request";
 export const UNAUTHORIZED_RESPONSE = "Unauthorized";
 export const PLEASE_LOGIN_RESPONSE = "Please Log in";
 export const USER_NOT_FOUND_RESPONSE = "User not found";
+export const EMAIL_EXISTS_RESPONSE = "Email already exists";
+export const USERNAME_EXISTS_RESPONSE = "Username already exists";
 export const PASSWORDS_DONT_MATCH_RESPONSE = "Passwords don't match";
