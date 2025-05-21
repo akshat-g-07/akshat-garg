1b8c7a48812d6fae4e270beda3f040bc90d403c3
Akshat Garg <akshatg805@gmail.com>
2025-05-21 16:03:32 +0530

    feat: tanstack query setup

--- apps/treklicious-fe/src/main.jsx
+++ apps/treklicious-fe/src/main.jsx
@@ -3,6 +3,14 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "@/routes";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
