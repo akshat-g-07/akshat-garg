eb8c3380b60561749235e932ca00fb63fbc8af18
Akshat Garg <akshatg805@gmail.com>
2025-05-21 12:08:26 +0530

    feat: signup controller

--- apps/treklicious-be/controllers/auth/index.js
+++ apps/treklicious-be/controllers/auth/index.js
@@ -1,5 +1,6 @@
 const Login = require("./login");
 const Logout = require("./logout");
 const Refresh = require("./refresh");
+const Signup = require("./signup");
 
-module.exports = { Login, Refresh, Logout };
+module.exports = { Login, Refresh, Logout, Signup };
--- apps/treklicious-be/routes/v1/auth.js
+++ apps/treklicious-be/routes/v1/auth.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const { Login, Refresh, Logout } = require("../../controllers/auth");
+const { Login, Refresh, Logout, Signup } = require("../../controllers/auth");
 const router = express.Router();
 
 router.post("/login", Login);
 router.get("/refresh", Refresh);
-// signup route
-router.post("/signup", () => {});
-// logout route
+router.post("/signup", Signup);
 router.post("/logout", Logout);
 
 module.exports = router;
