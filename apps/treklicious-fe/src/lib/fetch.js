2abfbb9e4c45632e9015de6314ee353aafb408af
Akshat Garg <akshatg805@gmail.com>
2025-05-21 17:27:16 +0530

    feat: fetch util

--- apps/treklicious-fe/src/lib/fetch.js
+++ apps/treklicious-fe/src/lib/fetch.js
@@ -1,4 +1,9 @@
 export default async function (...args) {
   const res = await fetch(...args);
-  return await res.json();
+  console.log("res fetch", res);
+
+  const json = await res.json();
+
+  console.log("res fetch1", json);
+  return json;
 }
