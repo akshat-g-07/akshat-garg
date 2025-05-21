50cc38038528f8d6b938ad10102ab51e0ee2ea53
Akshat Garg <akshatg805@gmail.com>
2025-05-21 17:52:06 +0530

    feat: all treks api

---
+++ apps/treklicious-fe/src/apis/Trek/AllTreks.js
@@ -0,0 +1,9 @@
+import fetch from "@/lib/fetch";
+import { TREK_ROUTE } from "@repo/treklicious-constants";
+import { apiv1, baseURL } from "../apiConstants";
+
+export default async function AllTreks() {
+  const url = baseURL + apiv1 + TREK_ROUTE;
+  const res = await fetch(url);
+  return res;
+}
