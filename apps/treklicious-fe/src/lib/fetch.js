32b59b11a1a7ddc1e9bfe1a90d3f9319bbf04c69
Akshat Garg <akshatg805@gmail.com>
2025-05-21 18:39:57 +0530

    feat: request id

--- apps/treklicious-fe/src/lib/fetch.js
+++ apps/treklicious-fe/src/lib/fetch.js
@@ -1,4 +1,15 @@
-export default async function (...args) {
-  const res = await fetch(...args);
+import { customAlphabet } from "nanoid";
+
+export default async function (url, options = {}) {
+  const nanoid = customAlphabet("1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5);
+  const requestID = nanoid();
+
+  const res = await fetch(url, {
+    ...options,
+    headers: {
+      ...(options.headers || {}),
+      "request-id": requestID,
+    },
+  });
   return await res.json();
 }
