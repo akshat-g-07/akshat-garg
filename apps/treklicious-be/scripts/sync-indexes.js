19dcf6dcb998e5076ca322f7b0437cf221f2652c
Akshat Garg <akshatg805@gmail.com>
2025-05-23 23:05:06 +0530

    chores: logs for better understanding

--- apps/treklicious-be/scripts/sync-indexes.js
+++ apps/treklicious-be/scripts/sync-indexes.js
@@ -11,12 +11,16 @@ const dbConnect = require("../config/db-connect");
 
 async function main() {
   await dbConnect();
+  console.log("Connected");
 
   await userIDModel.syncIndexes();
   await userDetailsModel.syncIndexes();
   await trekDetailsModel.syncIndexes();
 
+  console.log("Success, Disconnecting!");
+
   await mongoose.disconnect();
+  console.log("Disconnected");
 }
 
 if (NODE_ENV !== "production") {
