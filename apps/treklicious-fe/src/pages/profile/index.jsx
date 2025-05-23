147e26176c1f04fe0d5beebdff03c36e016cda41
Akshat Garg <akshatg805@gmail.com>
2025-05-24 00:07:45 +0530

    feat: get profile api at profile page

--- apps/treklicious-fe/src/pages/profile/index.jsx
+++ apps/treklicious-fe/src/pages/profile/index.jsx
@@ -1,20 +1,41 @@
+import { APIs } from "@/apis";
 import SettingSetup from "@/components/common/setting-setup";
 import PreferencesForm from "@/components/profile/preferences-form";
 import ProfileForm from "@/components/profile/profile-form";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Profile() {
+  const queryKey = "get-profile";
+  const { queryOptions, meta } = APIs[queryKey];
+  const {
+    isLoading,
+    error,
+    data: Profile,
+  } = useQuery({
+    queryKey: [queryKey],
+    ...queryOptions,
+    meta,
+  });
+
+  if (error) {
+    console.log("Error in Profile", error);
+    return <></>;
+  }
+
   return (
     <SettingSetup>
       <ProfileForm
-        defaultFirstName={"defaultFirstName"}
-        defaultLastName={"defaultLastName"}
-        defaultEmail={"defaultEmail"}
-        defaultUserName={"defaultUserName"}
+        isLoading={isLoading}
+        defaultFirstName={Profile?.firstName}
+        defaultLastName={Profile?.lastName}
+        defaultEmail={Profile?.email}
+        defaultUserName={Profile?.userName}
       />
       <PreferencesForm
-        defaultState={"Uttarakhand"}
-        defaultSeason={"Summer"}
-        defaultDifficulty={"Easy"}
+        isLoading={isLoading}
+        defaultState={Profile?.preferences.state}
+        defaultSeason={Profile?.preferences.season}
+        defaultDifficulty={Profile?.preferences.difficulty}
       />
     </SettingSetup>
   );
