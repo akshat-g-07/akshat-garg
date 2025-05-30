a3b0bff228091b34185b76a25886c6e16c023cd8
Akshat Garg <akshatg805@gmail.com>
2025-05-30 16:31:34 +0530

    feat: auth alert

---
+++ apps/treklicious-fe/src/components/common/auth-alert.jsx
@@ -0,0 +1,47 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useLocation, useNavigate } from "react-router";
+
+export default function AuthAlert() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const to = location.key === "default" ? "/" : -1;
+
+  return (
+    <AlertDialog defaultOpen>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Please Log In</AlertDialogTitle>
+          <AlertDialogDescription>
+            You’ll need to log in to proceed.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            onClick={() => {
+              navigate(to);
+            }}
+          >
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              navigate("/log-in");
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
