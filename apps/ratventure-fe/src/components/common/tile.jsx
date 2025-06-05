b1cf4cfdac588459ba570bdca973d5e21b2fa0c0
Akshat Garg <akshatg805@gmail.com>
2025-06-05 22:32:57 +0530

    feat: common tile component

---
+++ apps/ratventure-fe/src/components/common/tile.jsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+
+export default function Tile({
+  src,
+  alt,
+  highlighted = false,
+  className,
+  ...props
+}) {
+  return (
+    <div
+      className={cn(
+        "size-[50px] border border-black m-[2.5px] bg-[radial-gradient(transparent_75%,_#000)] hover:scale-105 outline-0 hover:outline-[2.5px] outline-blue-500 cursor-pointer ease-in-out transition-all duration-100 flex justify-center items-center group/tile",
+        className
+      )}
+      {...props}
+    >
+      <img
+        src={src}
+        alt={alt}
+        className={cn(
+          "object-fit opacity-0 group-hover/tile:opacity-75",
+          highlighted && "opacity-100 group-hover/tile:opacity-100"
+        )}
+      />
+    </div>
+  );
+}
