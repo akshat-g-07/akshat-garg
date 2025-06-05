61917dd4b68a5894624eff661e8146618cfa0347
Akshat Garg <akshatg805@gmail.com>
2025-06-05 23:12:39 +0530

    feat: responsive

--- apps/ratventure-fe/src/components/common/tile.jsx
+++ apps/ratventure-fe/src/components/common/tile.jsx
@@ -3,14 +3,14 @@ import { cn } from "@/lib/utils";
 export default function Tile({
   src,
   alt,
-  highlighted = false,
   className,
+  highlighted = false,
   ...props
 }) {
   return (
     <div
       className={cn(
-        "size-[50px] border border-black m-[2.5px] bg-[radial-gradient(transparent_75%,_#000)] hover:scale-105 outline-0 hover:outline-[2.5px] outline-blue-500 cursor-pointer ease-in-out transition-all duration-100 flex justify-center items-center group/tile",
+        "size-[25px] md:size-[50px] border border-black m-px md:m-[2.5px] bg-[radial-gradient(transparent_75%,_#000)] hover:scale-105 outline-0 hover:outline-[2.5px] outline-blue-500 cursor-pointer ease-in-out transition-all duration-100 flex justify-center items-center group/tile",
         className
       )}
       {...props}
