581b5128cb2eb8945cd520908e1079e6088f899b
Akshat Garg <akshatg805@gmail.com>
2025-06-12 12:30:50 +0530

    feat: ratventure server

---
+++ apps/ratventure-be/engine/solve-recursion.js
@@ -0,0 +1,128 @@
+const IsSafe = require("./is-safe");
+
+function SolveRecursion({
+  matrixSize,
+  startRow,
+  startCol,
+  blocksRowsArr,
+  blocksColsArr,
+  endRow,
+  endCol,
+  solution,
+  visitedRow,
+  visitedCol,
+}) {
+  if (startRow === endRow && startCol === endCol) {
+    solution.solutionRow.push(visitedRow.slice());
+    solution.solutionCol.push(visitedCol.slice());
+    return true;
+  }
+
+  visitedRow.push(startRow);
+  visitedCol.push(startCol);
+
+  if (
+    IsSafe({
+      matrixSize,
+      currRow: startRow + 1,
+      currCol: startCol,
+      blocksRowsArr,
+      blocksColsArr,
+      visitedRow,
+      visitedCol,
+    })
+  ) {
+    SolveRecursion({
+      matrixSize,
+      startRow: startRow + 1,
+      startCol,
+      blocksRowsArr,
+      blocksColsArr,
+      endRow,
+      endCol,
+      solution,
+      visitedRow,
+      visitedCol,
+    });
+  }
+
+  if (
+    IsSafe({
+      matrixSize,
+      currRow: startRow - 1,
+      currCol: startCol,
+      blocksRowsArr,
+      blocksColsArr,
+      visitedRow,
+      visitedCol,
+    })
+  ) {
+    SolveRecursion({
+      matrixSize,
+      startRow: startRow - 1,
+      startCol,
+      blocksRowsArr,
+      blocksColsArr,
+      endRow,
+      endCol,
+      solution,
+      visitedRow,
+      visitedCol,
+    });
+  }
+
+  if (
+    IsSafe({
+      matrixSize,
+      currRow: startRow,
+      currCol: startCol + 1,
+      blocksRowsArr,
+      blocksColsArr,
+      visitedRow,
+      visitedCol,
+    })
+  ) {
+    SolveRecursion({
+      matrixSize,
+      startRow,
+      startCol: startCol + 1,
+      blocksRowsArr,
+      blocksColsArr,
+      endRow,
+      endCol,
+      solution,
+      visitedRow,
+      visitedCol,
+    });
+  }
+
+  if (
+    IsSafe({
+      matrixSize,
+      currRow: startRow,
+      currCol: startCol - 1,
+      blocksRowsArr,
+      blocksColsArr,
+      visitedRow,
+      visitedCol,
+    })
+  ) {
+    SolveRecursion({
+      matrixSize,
+      startRow,
+      startCol: startCol - 1,
+      blocksRowsArr,
+      blocksColsArr,
+      endRow,
+      endCol,
+      solution,
+      visitedRow,
+      visitedCol,
+    });
+  }
+
+  visitedRow.pop();
+  visitedCol.pop();
+}
+
+module.exports = SolveRecursion;
--- apps/ratventure-be/index.js
+++ apps/ratventure-be/index.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 3600;
 const cors = require("cors");
 const corsOptions = require("./config/cors-options");
 
+const Solve = require("./controller/solve");
+
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.status(200).send("Running");
 });
+
+app.get("/solve", Solve);
 
 app.listen(port, async () => {
   console.log(`🚀🚀🚀App listening on port ${port}🚀🚀🚀`);
