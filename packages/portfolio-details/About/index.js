04443b7b011c597ea6c1ea7c46bc4d91c0f1e2ee
Akshat Garg <akshatg805@gmail.com>
2025-12-05 15:12:50 +0530

    update: english

--- packages/portfolio-details/About/index.js
+++ packages/portfolio-details/About/index.js
@@ -1,3 +1,3 @@
-const About = `Hey there!\nI am Akshat Garg, alumnus of NIT Patna. I am an experienced full stack developer proficient with MERN and specialized in React ecosystem.\nI have worked for Fortune 500 clients like Apple, Walmart, P&G.\nI really thrive to develop something meaningful!`;
+const About = `Hey there!\nI am Akshat Garg, alumnus of NIT Patna. I am an experienced full stack developer proficient with MERN, ensuring end-to-end ownership.\nI have worked for Fortune 500 clients like Apple, Walmart, P&G.\nI really thrive to develop something meaningful!`;
 
 export default About;
